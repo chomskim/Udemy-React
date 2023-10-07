a1beee7abbf0acc981fe857b5391f498a62491b8
chomskim <csknova@gmail.com>
2023-10-07 21:59:48 +0900

    add more project

---
+++ my-build.md
@@ -0,0 +1,14 @@
+# Recreate react-course-comfy-sloth-store using Vite
+
+```
+npm create vite@latest react-vite-comfy-sloth -- --template react
+cd react-vite-comfy-sloth
+
+copy some packages package.json
+npm i
+
+
+copy index.js --> main.jsx
+copy App.js --> App.jsx
+
+```
---
+++ react-vite-comfy-sloth/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ProductsProvider } from './context/products_context'
+import { FilterProvider } from './context/filter_context'
+import { CartProvider } from './context/cart_context'
+import { UserProvider } from './context/user_context'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={import.meta.env.REACT_APP_AUTH_DOMAIN}
+    clientId={import.meta.env.REACT_APP_CLIENT_ID}
+    authorizationParams={{
+      redirect_uri: `${window.location.origin}/cart`,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+)
